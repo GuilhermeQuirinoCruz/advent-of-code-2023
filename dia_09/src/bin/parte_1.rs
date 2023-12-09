c5c672d13dc23bbca2dedfdee534921487152b20
GuilhermeQuirinoCruz <guilhermequirinocruz@gmail.com>
2023-12-09 17:30:24 -0300

    dia 9 parte 1

---
+++ dia_09/src/bin/parte_1.rs
@@ -0,0 +1,41 @@
+fn predict_next_value(history: &Vec<i32>) -> i32 {
+    let mut differences: Vec<i32> = Vec::new();
+    differences.extend(history);
+
+    let mut last_values: Vec<i32> = Vec::new();
+
+    while !differences.iter().all(|&d| d.eq(&0)) {
+        for i in 0..(differences.len() - 1) {
+            differences[i] = differences[i + 1] - differences[i];
+        }
+
+        last_values.push(*differences.last().unwrap());
+
+        differences.pop();
+        // println!("{:?}", differences);
+    }
+    // println!("{:?}", last_values);
+    return last_values.iter().sum::<i32>();
+}
+
+fn main() {
+    // let input: &str = include_str!("./exemplo_1.txt");
+    let input: &str = include_str!("./input.txt");
+
+    let histories: Vec<Vec<i32>> = input
+        .lines()
+        .map(|line| {
+            line.split_whitespace()
+                .into_iter()
+                .map(|v| v.parse::<i32>().unwrap())
+                .collect()
+        })
+        .collect();
+
+    let sum_extrapolated: i32 = histories
+        .iter()
+        .map(|history| predict_next_value(history))
+        .sum::<i32>();
+
+    println!("{}", sum_extrapolated);
+}
