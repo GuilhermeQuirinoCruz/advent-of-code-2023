2ad13c12bac939539f0e13f6c2c03cbd64b5b357
GuilhermeQuirinoCruz <guilhermequirinocruz@gmail.com>
2023-12-15 11:04:22 -0300

    dia 15 parte 1

---
+++ dia_15/src/bin/parte_1.rs
@@ -0,0 +1,33 @@
+fn get_ascii_code(c: char) -> u32 {
+    return c as u32;
+}
+
+fn get_hash(str: &str) -> u32 {
+    let mut hash: u32 = 0;
+
+    for c in str.chars() {
+        hash += get_ascii_code(c);
+        hash *= 17;
+        hash %= 256;
+    }
+
+    return hash;
+}
+
+fn main() {
+    // let input: &str = include_str!("./exemplo_1.txt");
+    let input: &str = include_str!("./input.txt");
+
+    // for str in input.split(",") {
+    //     println!("{}", get_hash(str));
+    // }
+
+    let sum: u32 = input
+        .split(",")
+        .map(|str| get_hash(str))
+        .collect::<Vec<u32>>()
+        .iter()
+        .sum::<u32>();
+
+    println!("{}", sum);
+}
