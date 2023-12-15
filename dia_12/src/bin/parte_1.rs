176c25897a5e83987b6aced8d88de05ffbf662ea
GuilhermeQuirinoCruz <guilhermequirinocruz@gmail.com>
2023-12-15 10:32:18 -0300

    dia 12 min e max

--- dia_12/src/bin/parte_1.rs
+++ dia_12/src/bin/parte_1.rs
@@ -1,4 +1,16 @@
 fn main() {
     let input: &str = include_str!("./exemplo_1.txt");
     // let input: &str = include_str!("./input.txt");
+
+    let spring_size = input
+        .lines()
+        .map(|l| l.split_whitespace().next().unwrap())
+        .map(|springs| springs.matches("?").count())
+        .collect::<Vec<usize>>();
+
+    println!(
+        "min {:?}, max {:?}",
+        spring_size.iter().min().unwrap(),
+        spring_size.iter().max().unwrap()
+    );
 }
